7b64f8fb69e7e792dfb64bb17bcfb6470086ee88
Judy Zhang <qianzhiz@usc.edu>
2018-06-06 20:00:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const router = require('./router.js');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    router.home(req, res);
+    router.user(req, res);
+}).listen(port, () => {
+    console.log(`Server running at http://:${port}/`);
+});
