2f70b3f6e200a6d737e1b6b2dcf93e16dc377be9
Judy Zhang <qianzhiz@usc.edu>
2018-06-06 22:29:27 -0700

    ...

--- web/access_mongoDB.js
+++ web/access_mongoDB.js
@@ -14,9 +14,10 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.createCollection("customers", function(err, res) {
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
         if (err) throw err;
-        console.log("Collection created!");
+        console.log("1 document inserted");
         db.close();
     });
 });
--- web/server.js
+++ web/server.js
@@ -3,26 +3,19 @@ var fs = require('fs');
 var url = require('url');
 
 http.createServer( function (request, response) {
-    // Parse the request containing file name
-    var pathname = url.parse(request.url).pathname;
+    var pathname = url.parse(request.url).pathname; //parsing the url
 
-    // Print the name of the file for which request is made.
     console.log("Request for " + pathname + " received.");
 
-    // Read the requested file content from file system
     fs.readFile(pathname.substr(1), function (err, data) {
         if (err) {
             console.log(err);
             response.writeHead(404, {'Content-Type': 'text/html'});
-        } else {
-            // Page found, HTTP Status: 200 : OK
-            // Content Type: text/plain
+        }
+        else {
             response.writeHead(200, {'Content-Type': 'text/html'});
-
-            // Write the content of the file to response body
             response.write(data.toString());
         }
-        // Send the response body
         response.end();
     });
 }).listen(8081);
